b65f3042d91735d406c2301d2d88eaa3a9378b54
Vudung201 <1911505310109@sv.ute.udn.vn>
2021-10-30 15:38:21 +0700

    merge homepage

--- app/src/main/java/com/example/team_project/Splash.java
+++ app/src/main/java/com/example/team_project/Splash.java
@@ -22,8 +22,7 @@ public class Splash extends AppCompatActivity {
             public void run() {
                 Intent in = new Intent(Splash.this,MainActivity.class);
                 startActivity(in);
-                finish();
             }
-        },5000);
+        },1000);
     }
 }
