75e4a34cb3500d75f1ae9af998adf022b8e34f70
Vudung201 <1911505310109@sv.ute.udn.vn>
2021-10-30 13:33:48 +0700

    merge homepage

--- app/src/main/java/com/example/team_project/MainActivity.java
+++ app/src/main/java/com/example/team_project/MainActivity.java
@@ -1,22 +1,32 @@
 package com.example.team_project;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
     LinearLayout change;
+    Button btn_login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
         change = (LinearLayout) findViewById(R.id.change);
-
+        btn_login = findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, homepage_Activity.class);
+                startActivity(intent);
+            }
+        });
         change.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- app/src/main/java/com/example/team_project/Mic_Login.java
+++ app/src/main/java/com/example/team_project/Mic_Login.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class Mic_Login extends AppCompatActivity {
     ImageButton imgBtn;
+    Button btn_Miclogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +23,14 @@ public class Mic_Login extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 backLogin();
+            }
+        });
+        btn_Miclogin = findViewById(R.id.btn_micLogin);
+        btn_Miclogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Mic_Login.this, homepage_Activity.class);
+                startActivity(intent);
             }
         });
     }
