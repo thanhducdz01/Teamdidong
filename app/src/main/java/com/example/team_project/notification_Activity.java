53765ce1371c30237641d72b867a14249097db66
Vudung201 <1911505310109@sv.ute.udn.vn>
2021-11-04 15:05:14 +0700

    update

--- app/src/main/java/com/example/team_project/notification_Activity.java
+++ app/src/main/java/com/example/team_project/notification_Activity.java
@@ -7,15 +7,20 @@ import android.view.View;
 import android.view.Window;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 public class notification_Activity extends AppCompatActivity {
     ImageButton homepage,btn_chatbox,btn_studentInfo;
     ImageView viewDetailNoti;
+    RelativeLayout btn_noti_details,btn_class_details,btn_pdt_details;
     void getView() {
         homepage = findViewById(R.id.img_button_homepage);
         btn_chatbox = findViewById(R.id.img_button_chatbox);
         btn_studentInfo= findViewById(R.id.img_button_student);
         viewDetailNoti = findViewById(R.id.detail_noti);
+        btn_noti_details = findViewById(R.id.btn_noti_details);
+        btn_class_details = findViewById(R.id.btn_class_details);
+        btn_pdt_details= findViewById(R.id.btn_pdt_details);
     }
 
     @Override
@@ -25,6 +30,30 @@ public class notification_Activity extends AppCompatActivity {
         getSupportActionBar().hide(); //hide the title bar
         setContentView(R.layout.notification);
         getView();
+
+        btn_noti_details.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(notification_Activity.this,notification_details_Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_class_details.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(notification_Activity.this,notification_details_Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_pdt_details.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(notification_Activity.this,notification_details_Activity.class);
+                startActivity(intent);
+            }
+        });
 
         viewDetailNoti.setOnClickListener(new View.OnClickListener() {
             @Override
