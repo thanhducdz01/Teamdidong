0d267ed75948cad432fbc5cb465e167fb8937775
Vudung201 <1911505310109@sv.ute.udn.vn>
2022-01-06 10:45:57 +0700

    vudung

--- app/src/main/java/com/example/team_project/homepage_Activity.java
+++ app/src/main/java/com/example/team_project/homepage_Activity.java
@@ -1,5 +1,6 @@
 package com.example.team_project;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +10,33 @@ import android.view.WindowManager;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 
 public class homepage_Activity extends AppCompatActivity {
@@ -19,6 +47,9 @@ public class homepage_Activity extends AppCompatActivity {
     LinearLayout btn_tkb;
     LinearLayout btn_lichthi;
     LinearLayout btn_ketqua;
+    TextView txtInfo;
+    String maSV="1911505310109";
+    private static final String URLgetProfile= "http://192.168.0.103/UTEapp/getProfile.php";
 
     void getView() {
         btn_info_student = findViewById(R.id.img_button_student);
@@ -39,10 +70,12 @@ public class homepage_Activity extends AppCompatActivity {
         getSupportActionBar().hide(); //hide the title bar
         setContentView(R.layout.activity_homepage);
         getView();
+        AddSimpleProfile();
         btn_info_student.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(homepage_Activity.this, student_information.class);
+                intent.putExtra("maSV",maSV);
                 startActivity(intent);
             }
         });
@@ -92,11 +125,92 @@ public class homepage_Activity extends AppCompatActivity {
         });
     }
 
+    private void AddSimpleProfile(){
+        StringRequest request = new StringRequest(Request.Method.POST, URLgetProfile, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    String result = jsonObject.getString("status");
+                    JSONArray jsonArray = jsonObject.getJSONArray("data");
+                    System.out.println(result);
+                    if (result.equals("success")){
+                        for (int i = 0; i<jsonArray.length(); i++){
+                            JSONObject object = jsonArray.getJSONObject(i);
+                            String maSV = object.getString("maSV");
+                            String tenSV = object.getString("tenSinhVien");
+                            String tenLop = object.getString("tenLop");
+                            String tenNganh = object.getString("tenNganh");
+                            String tenKhoa = object.getString("tenKhoa");
+                            String gioiTinh = object.getString("gioiTinh");
+                            String email = object.getString("email");
+                            String soDienThoai = object.getString("soDienThoai");
+                            String diaChi = object.getString("diaChi");
+                            String matKhauTK = object.getString("matKhauTK");
+                            String matKhauEMAIL = object.getString("matKhauEMAIL");
+                            txtInfo = findViewById(R.id.textview_information);
+                            txtInfo.setText(tenSV+"\n"+maSV+"\n"+tenLop+"\n"+tenKhoa);
+
+                        }
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(),error.toString(),Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> pr = new HashMap<String,String>();
+                pr.put("maSV",maSV);
+                return pr;
+            }
+        };
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        queue.add(request);
+    }
+
     public void onBackPressed() {
         Intent startMain = new Intent(Intent.ACTION_MAIN);
         startMain.addCategory(Intent.CATEGORY_HOME);
         startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(startMain);
+    }
+    /**
+     * Enables https connections
+     */
+    @SuppressLint("TrulyRandom")
+    public static void handleSSLHandshake() {
+        try {
+            TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
+                public X509Certificate[] getAcceptedIssuers() {
+                    return new X509Certificate[0];
+                }
+
+                @Override
+                public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                }
+
+                @Override
+                public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                }
+            }};
+
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+            HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
+                @Override
+                public boolean verify(String arg0, SSLSession arg1) {
+                    return true;
+                }
+            });
+        } catch (Exception ignored) {
+        }
     }
 
 }
--- app/src/main/java/com/example/team_project/student_information.java
+++ app/src/main/java/com/example/team_project/student_information.java
@@ -10,12 +10,30 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class student_information extends AppCompatActivity {
 
     ImageButton homepage,btn_chatbox,btn_notification,btn_logOut;
     Dialog popupStudent_Dialog;
+    TextView stuname,txt_msv,txt_khoa,txtlop,edtEmail,edtPhone,edtAddress,edtDate,edtGender;
+    private static final String URLgetProfile= "http://192.168.0.103/UTEapp/getProfile.php";
     void getView() {
         homepage = findViewById(R.id.img_button_homepage);
         btn_chatbox = findViewById(R.id.img_button_chatbox);
@@ -32,6 +50,9 @@ public class student_information extends AppCompatActivity {
         getSupportActionBar().hide(); //hide the title bar
         setContentView(R.layout.infomation_student);
         getView();
+        AddProfile();
+        Intent intentget = getIntent();
+        String maSV = intentget.getStringExtra("maSV");
         homepage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -66,6 +87,74 @@ public class student_information extends AppCompatActivity {
 
         popupStudent_Dialog = new Dialog(this);
 
+    }
+    private void AddProfile(){
+        Intent intentget = getIntent();
+        String maSV = intentget.getStringExtra("maSV");
+        StringRequest request = new StringRequest(Request.Method.POST, URLgetProfile, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    String result = jsonObject.getString("status");
+                    JSONArray jsonArray = jsonObject.getJSONArray("data");
+                    System.out.println(result);
+                    if (result.equals("success")){
+                        for (int i = 0; i<jsonArray.length(); i++){
+                            JSONObject object = jsonArray.getJSONObject(i);
+                            String maSV = object.getString("maSV");
+                            String tenSV = object.getString("tenSinhVien");
+                            String tenLop = object.getString("tenLop");
+                            String tenNganh = object.getString("tenNganh");
+                            String tenKhoa = object.getString("tenKhoa");
+                            String gioiTinh = object.getString("gioiTinh");
+                            String email = object.getString("email");
+                            String ngaySinh = object.getString("ngaySinh");
+                            String soDienThoai = object.getString("soDienThoai");
+                            String diaChi = object.getString("diaChi");
+                            String matKhauTK = object.getString("matKhauTK");
+                            String matKhauEMAIL = object.getString("matKhauEMAIL");
+                            stuname = findViewById(R.id.text_stuname);
+                            txt_msv = findViewById(R.id.text_msv);
+                            txt_khoa = findViewById(R.id.text_khoa);
+                            txtlop = findViewById(R.id.text_class);
+                            edtEmail = findViewById(R.id.text_cemail);
+                            edtPhone = findViewById(R.id.text_cphone);
+                            edtAddress = findViewById(R.id.text_caddress);
+                            edtDate = findViewById(R.id.text_cdate);
+                            edtGender = findViewById(R.id.text_cgender);
+
+                            stuname.setText("Xin chào, "+tenSV);
+                            txt_msv.setText("Mã sinh viên: "+maSV);
+                            txt_khoa.setText(tenKhoa);
+                            txtlop.setText("Lớp: "+tenLop);
+                            edtEmail.setText(email);
+                            edtPhone.setText(soDienThoai);
+                            edtAddress.setText(diaChi);
+                            edtDate.setText(ngaySinh);
+                            edtGender.setText(gioiTinh);
+
+                        }
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(),error.toString(),Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> pr = new HashMap<String,String>();
+                pr.put("maSV",maSV);
+                return pr;
+            }
+        };
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        queue.add(request);
     }
     public void ShowPopup(View v){
         Button btn_submitPopup, btn_closePopup;
