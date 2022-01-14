6e84450371a2364aa36aba6b2cdf7b05789fb72b
thanhducdz01 <phaithatthanhcong2k1@gmail.com>
2022-01-15 00:50:46 +0700

    test

--- app/src/main/java/com/example/team_project/tkb_t2.java
+++ app/src/main/java/com/example/team_project/tkb_t2.java
@@ -10,15 +10,24 @@ import androidx.fragment.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 public class tkb_t2 extends Fragment {
+
     View view;
+    TextView txttietBD ,txttietKT ,txtidPhong ,txtMonHoc;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view =  inflater.inflate(R.layout.fragment_tkb_t2, container, false);
+        View view =inflater.inflate(R.layout.fragment_tkb_t2,container,false);
+        txttietBD =(TextView) view.findViewById(R.id.tietBD);
+        txttietKT=(TextView) view.findViewById(R.id.tietKT);
+        txtidPhong=(TextView) view.findViewById(R.id.idPhong);
+        txtMonHoc =(TextView) view.findViewById(R.id.Monhoc);
+
         return  view;
     }
 
